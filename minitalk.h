cae173f9a9e0c0fd5d48d3d653df62844f9c2ce2
kobe <thodavid@student.42.fr>
2025-04-02 17:54:56 +0200

    Create minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/02 13:14:02 by thodavid          #+#    #+#             */
+/*   Updated: 2025/04/02 13:45:26 by thodavid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef MINITALK_H
+# define MINITALK_H
+
+#include "./ft_printf/ft_printf.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+
+#endif
