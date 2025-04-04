3cf90760a5e9b88f06d5434a49215e713cecb605
kobe <thodavid@student.42.fr>
2025-04-04 15:14:05 +0200

    Update client.c

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:02:30 by thodavid          #+#    #+#             */
-/*   Updated: 2025/04/02 17:53:12 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/04/04 14:56:48 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,51 +23,71 @@
 void handler(int sig)
 {
 	(void)sig;
-	printf("\nhandler serve function\n\n");
+	ft_printf("\nhandler serve function\n\n");
 }
 
-// !!!! free si besoin le return de print bit
-char *char_to_bit(char c)
+void	ft_sending_bit(pid_t pid_serv, char	*msg)
 {
 	int	i;
+	int	j;
 	char *bit;
 
-	i = 8;
-	bit = malloc(9);
-	if (bit == NULL)
-		return (NULL);
-	bit[8] = '\0';
-	while (i--)
-		bit[i] = (c >> i & 1) + '0';
-	return (bit);
-}
-
-long	ft_ascii_to_pid(const char *str)
-{
-	int			i;
-	long		r;
-//!!!!!!!!!!!!!!!!! attention verification si chaine vide si caratere alphabetique!!!!!!!!!!!!!!
 	i = 0;
-	r = 0;
-	while (str[i] >= '0' && str[i] <= '9')
+	while(msg[i])
 	{
-		r = (r * 10) + (str[i] - '0');
+		bit = char_to_bit(msg[i]);
+		if(bit == NULL)
+			ft_error("MALLOC err");
+		j = 0;
+		while(bit[j])
+		{
+			if(bit[j] == '1')
+			{
+				kill(pid_serv, SIGUSR1);
+				ft_printf("\n==1==\n");
+				sleep(1);
+			}
+			else
+			{
+				kill(pid_serv, SIGUSR2);
+				ft_printf("\n==0==\n");
+				sleep(1);
+			}
+			j++;
+		}
+		free(bit);
 		i++;
 	}
-	return (r);
+	
 }
 
+/*parsing des param (PID SERV + MSG)*/
+/*
+1- PARSING
+3- DECOMPOSER MESSAGE
+4- ENVOIE MESSAGE BIT/BIT
+*/
 int main(int ac, char **av)
 {
-	pid_t	pid;
+	int	i;
+	pid_t	pid_serv;
+	
+	i = 0;
+	/*1-PARSING*/
 	if (ac != 3)
+	{
+		ft_error("ARGs err");
 		return (0);
-	/*
-	1- WRITE PID
-	2- CHECK MESSAGE
-	3- DECOMPOSER MESSAGE
-	4- ENVOIE MESSAGE BIT/BIT
-	*/
-	pid = (pid_t)ft_ascii_to_pid(av[1]);
-	ft_printf("pid CLI: %d\n", getpid());
+	}
+	check_arg(av[1], av[2]);
+	pid_serv = (pid_t)ft_ascii_to_pid(av[1]);
+	/******************/
+	/*SENDING*/
+	ft_sending_bit(pid_serv, av[2]);
+	/******************/
+
+	ft_printf("PID SERV: %d\n", pid_serv);
+	ft_printf("PID CLI: %d\n", getpid());
+	ft_printf("message ==%s==\n", av[2]);
 }
+
