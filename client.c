4e2922dbffd89e43dc0631a564f31e9df7e7c4b6
kobe <thodavid@student.42.fr>
2025-04-06 13:58:05 +0200

    Update client.c

--- client.c
+++ client.c
@@ -6,74 +6,70 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 16:02:30 by thodavid          #+#    #+#             */
-/*   Updated: 2025/04/04 14:56:48 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/04/06 12:47:30 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-██████  ██      ██ ███████ ███    ██ ████████
-██      ██      ██ ██      ████   ██    ██
-██      ██      ██ █████   ██ ██  ██    ██
-██      ██      ██ ██      ██  ██ ██    ██
-██████  ███████ ██ ███████ ██   ████    ██
+   ██████  ██      ██ ███████ ███    ██ ████████
+   ██      ██      ██ ██      ████   ██    ██
+   ██      ██      ██ █████   ██ ██  ██    ██
+   ██      ██      ██ ██      ██  ██ ██    ██
+   ██████  ███████ ██ ███████ ██   ████    ██
 */
 
 #include "minitalk.h"
 
-void handler(int sig)
+volatile sig_atomic_t	g_ack_conf = 0;
+
+void	handle_siguser(int sig)
 {
-	(void)sig;
-	ft_printf("\nhandler serve function\n\n");
+	(void) sig;
+	g_ack_conf = 1;
 }
 
-void	ft_sending_bit(pid_t pid_serv, char	*msg)
+void	ft_sending_bit(pid_t pid_serv, char *msg)
 {
-	int	i;
-	int	j;
-	char *bit;
+	int			i;
+	int			j;
+	char		*bit;
 
 	i = 0;
-	while(msg[i])
+	while (i <= ft_strlen(msg))
 	{
 		bit = char_to_bit(msg[i]);
-		if(bit == NULL)
+		if (bit == NULL)
 			ft_error("MALLOC err");
 		j = 0;
-		while(bit[j])
+		while (bit[j])
 		{
-			if(bit[j] == '1')
-			{
+			g_ack_conf = 0;
+			if (bit[j] == '1')
 				kill(pid_serv, SIGUSR1);
-				ft_printf("\n==1==\n");
-				sleep(1);
-			}
 			else
-			{
 				kill(pid_serv, SIGUSR2);
-				ft_printf("\n==0==\n");
-				sleep(1);
-			}
+			while (!g_ack_conf)
+				usleep(50);
 			j++;
 		}
 		free(bit);
 		i++;
 	}
-	
 }
 
 /*parsing des param (PID SERV + MSG)*/
 /*
-1- PARSING
-3- DECOMPOSER MESSAGE
-4- ENVOIE MESSAGE BIT/BIT
+   1- PARSING (check arg + translate the pid param)
+   3- DECOMPOSER MESSAGE
+   4- ENVOIE MESSAGE BIT/BIT
 */
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int	i;
-	pid_t	pid_serv;
-	
+	struct sigaction	sa;
+	pid_t				pid_serv;
+	int					i;
+
 	i = 0;
-	/*1-PARSING*/
 	if (ac != 3)
 	{
 		ft_error("ARGs err");
@@ -81,13 +77,10 @@ int main(int ac, char **av)
 	}
 	check_arg(av[1], av[2]);
 	pid_serv = (pid_t)ft_ascii_to_pid(av[1]);
-	/******************/
-	/*SENDING*/
+	sa.sa_handler = &handle_siguser;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGUSR1, &sa, NULL);
 	ft_sending_bit(pid_serv, av[2]);
-	/******************/
-
-	ft_printf("PID SERV: %d\n", pid_serv);
-	ft_printf("PID CLI: %d\n", getpid());
-	ft_printf("message ==%s==\n", av[2]);
+	return (0);
 }
-
