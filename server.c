6706a6dacae94f906e5b0da6a9022940161b0c6a
kobe <thodavid@student.42.fr>
2025-04-06 13:59:11 +0200

    Update server.c

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: thodavid <thodavid@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/28 12:51:14 by thodavid          #+#    #+#             */
-/*   Updated: 2025/04/04 15:07:39 by thodavid         ###   ########.fr       */
+/*   Updated: 2025/04/06 12:23:47 by thodavid         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,44 +20,38 @@
 
 #include "minitalk.h"
 
-void handler(int sig)
+void	handle_siguser(int sig, siginfo_t *info, void *context)
 {
-	(void)sig;
-	ft_printf("\nhandler serve function\n\n");
-}
+	static char	c;
+	static int	i;
 
-c handle_siguser(int sig)
-{
+	(void) context;
 	if (sig == SIGUSR1)
+		c |= (1 << (7 - i));
+	i++;
+	if (i == 8)
 	{
-		
-		printf("\nbits is ==1==\n");
+		if (c == '\0')
+			write(1, "\n", 1);
+		else
+			ft_putchar_fd(c, 1);
+		i = 0;
+		c = 0;
 	}
-	if	(sig == SIGUSR2)
-	{
-		
-		printf("\nbits is ==0==\n");
-	}
+	kill(info->si_pid, SIGUSR1);
 }
 
-int main(void)
+int	main(void)
 {
-	struct sigaction sa;
-	
-	sa.sa_handler = &handle_siguser;
+	struct sigaction	sa;
+
+	sa.sa_sigaction = &handle_siguser;
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = 0;
-	
+	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	ft_printf("PID SERV: %d\n", getpid());
 	while (1)
-	{
-		sleep(2);
-		ft_printf("SERVER ACTIVE...");
-	}
-	
-
+		pause();
 	return (0);
 }
-
